19ca06ef04e831624f302417772ba940c89378b7
Agustin Yedro <76633203+agustinyedro@users.noreply.github.com>
2026-01-27 17:55:11 -0300

    se modifican config de carpetas

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,10 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-  }));
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
